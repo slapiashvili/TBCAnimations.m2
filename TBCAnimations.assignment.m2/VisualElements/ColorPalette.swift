f7d7087e22a4bbde7bbca44f89c9c54cb4b9aa5c
Salome Lapiashvili <salomelapiashvili@Salomes-MacBook-Air.local>
2023-11-13 02:17:39 +0400

    first commit

---
+++ TBCAnimations.assignment.m2/VisualElements/ColorPalette.swift
@@ -0,0 +1,30 @@
+//
+//  ColorPalette.swift
+//  TBCAnimations.assignment.m2
+//
+//  Created by Salome Lapiashvili on 13.11.23.
+//
+
+import UIKit
+
+extension UIColor {
+    static var tbcBlue: UIColor {
+        return UIColor(red: 0x00 / 255.0, green: 0xAE / 255.0, blue: 0xEF / 255.0, alpha: 1.0)
+    }
+    
+    static var tbcYellow: UIColor {
+        return UIColor(red: 0xEC / 255.0, green: 0xEE / 255.0, blue: 0x0F / 255.0, alpha: 1.0)
+    }
+    
+    static var tbcGreen: UIColor {
+        return UIColor(red: 0x21 / 255.0, green: 0xF0 / 255.0, blue: 0x02 / 255.0, alpha: 1.0)
+    }
+    
+    static var tbcPurple: UIColor {
+        return UIColor(red: 0x26 / 255.0, green: 0x1A / 255.0, blue: 0xD6 / 255.0, alpha: 1.0)
+    }
+    
+    static var tbcRed: UIColor {
+        return UIColor(red: 0xB5 / 255.0, green: 0x15 / 255.0, blue: 0x09 / 255.0, alpha: 1.0)
+    }
+}
---
+++ TBCAnimations.assignment.m2UITests/TBCAnimations_assignment_m2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TBCAnimations_assignment_m2UITestsLaunchTests.swift
+//  TBCAnimations.assignment.m2UITests
+//
+//  Created by Salome Lapiashvili on 13.11.23.
+//
+
+import XCTest
+
+final class TBCAnimations_assignment_m2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
